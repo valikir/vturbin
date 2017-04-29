7a3115a1d50ac232b4e88e8500a5ebc14d4db77a
valikir <v.turbin@gmx.de>
2017-04-29 11:16:51 +0200

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j;
 
+/**
+* Hello World code
+*
+* @author Valentin Turbin 
+* @version 1
+* @since 29.04.2017
+* @class Calculate
+*/
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
