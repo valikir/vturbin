f3405f195d8659f595ff82eff7cbf1ce615af049
valikir <v.turbin@gmx.de>
2017-04-30 16:41:48 +0200

    task 185

---
+++ chapter_001/src/test/java/ru/job4j/CalculatorTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+	@Test
+    public void whenSubstractOneMinusOneThenZero() {
+        Calculator calc = new Calculator();
+        calc.substract(1D, 1D);
+        double result = calc.getResult();
+        double expected = 0;
+        assertThat(result, is(expected));
+    }
+	@Test
+    public void whenDivideFourByTwoThenTwo() {
+        Calculator calc = new Calculator();
+        calc.div(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+	@Test
+    public void whenMultiplyFourByFourThenSixteen() {
+        Calculator calc = new Calculator();
+        calc.multiple(4D, 4D);
+        double result = calc.getResult();
+        double expected = 16D;
+        assertThat(result, is(expected));
+    }
+}
