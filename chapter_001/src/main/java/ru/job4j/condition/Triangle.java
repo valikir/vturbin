2f967e4a958472b76336723d7a02e32fce6aab8a
valikir <v.turbin@gmx.de>
2017-05-04 20:45:59 +0200

    task 9461

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,10 +1,33 @@
 package ru.job4j.condition;
 
-
+/**
+* Find an area of a triangle.
+*
+* @author Valentin Turbin
+* @version 1
+* @since 4.05.2017
+*/
 public class Triangle {
+	/**
+	* координаты точки a.
+	*/
   private Point a;
+	/**
+	* координаты точки b.
+	*/
   private Point b;
+	/**
+	* координаты точки c.
+	*/
   private Point c;
+
+/**
+* Triangle class.
+*
+* @param a координаты точки а
+* @param b координаты точки b
+* @param c координаты точки c
+*/
 
   public Triangle(Point a, Point b, Point c) {
     this.a = a;
@@ -12,8 +35,17 @@ public class Triangle {
     this.c = c;
   }
 
+/**
+* Find area.
+* @return area возвращает площадь треугольника
+*/
   public double area() {
 //calculate the triangle area.
-    return Math.abs((a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())) / 2);
+	double area = Math.abs((a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())) / 2D);
+	if (area == 0) {
+		return -1;
+	} else {
+		return area;
+	}
   }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -13,13 +13,30 @@ import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.number.IsCloseTo.closeTo;
 
+/**
+* Triangle test class.
+*/
+
 public class TriangleTest {
-   @Test
+	/**
+	* Triangle test class.
+	*/
+
    private Point a = new Point(15, 15);
+	/**
+	* Triangle test point b.
+	*/
    private Point b = new Point(15, 30);
+	/**
+	* Triangle test point c.
+	*/
    private Point c = new Point(50, 25);
+   /**
+	* Test area of a triangle.
+	*/
+	@Test
     public void ifPointsGivenFindArea() {
-        Triangle triangle1 = Triangle(Point a, Point b, Point c);
+        Triangle triangle1 =  new Triangle(a, b, c);
 		double result = triangle1.area();
 		double expect = 262.5D;
         assertThat(result, closeTo(expect, 0.01));
