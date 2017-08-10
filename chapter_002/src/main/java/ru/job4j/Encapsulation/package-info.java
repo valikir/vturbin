d16d2a91c6ee6018a7a0b0724be8b75180e4dfc9
valikir <v.turbin@gmx.de>
2017-08-10 12:21:59 +0200

    Task #784

---
+++ chapter_002/src/main/java/ru/job4j/Encapsulation/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Valentin Turbin
+* @version 1
+* @since 07.07.2017
+*/
+package ru.job4j.Encapsulation;
---
+++ chapter_002/src/main/java/ru/job4j/Polymorphism/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by vturb on 10-Aug-17.
+ */
+package ru.job4j.Polymorphism;
