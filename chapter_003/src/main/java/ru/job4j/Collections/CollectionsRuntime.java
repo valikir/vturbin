4f44af193e457645acb17b4bfaf880542983a49d
valikir <v.turbin@gmx.de>
2017-09-02 19:04:59 +0200

    Тестирование производительности коллекций. Task #10022

--- chapter_003/src/main/java/ru/job4j/Collections/CollectionsRuntime.java
+++ chapter_003/src/main/java/ru/job4j/Collections/CollectionsRuntime.java
@@ -24,8 +24,12 @@ public class CollectionsRuntime {
 
     public long delete(Collection<String> collection, int amount) {
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < amount; i++) {
-            collection.remove(i);
+        int count = 0;
+        Iterator<String> iterator = collection.iterator();
+        while(iterator.hasNext() && count != amount) {
+            iterator.next();
+            iterator.remove();
+            count++;
         }
         long endTime = System.currentTimeMillis();
         long period = (endTime - startTime);
@@ -36,26 +40,28 @@ public class CollectionsRuntime {
             CollectionsRuntime cr = new CollectionsRuntime();
 
             //LinkedList
-            LinkedList<String> list = new LinkedList<>();
-            long timeForAdd = cr.add(list,100);
-            long timeForDelete = cr.delete(list,10);
-            //System.out.println(list);
+            List<String> list = new LinkedList<>();
+            long timeForAdd = cr.add(list,10000);
+            long timeForDelete = cr.delete(list,100);
+            System.out.println("LinkedList");
             System.out.println("That took " + timeForAdd + " milliseconds for add operation");
-            System.out.println("That took " + timeForDelete+ " milliseconds for add operation");
+            System.out.println("That took " + timeForDelete+ " milliseconds for delete operation");
 
             //ArrayList
             List<String> list2 = new ArrayList<>();
-        timeForAdd = cr.add(list2,100);
-        timeForDelete = cr.delete(list2,10);
+        timeForAdd = cr.add(list2,10000);
+        timeForDelete = cr.delete(list2,100);
+        System.out.println("ArrayList");
         System.out.println("That took " + timeForAdd + " milliseconds for add operation");
-        System.out.println("That took " + timeForDelete+ " milliseconds for add operation");
+        System.out.println("That took " + timeForDelete+ " milliseconds for delete operation");
 
         //TreeSet
         Set<String> set = new TreeSet<>();
-        timeForAdd = cr.add(set,100);
-        timeForDelete = cr.delete(set,10);
+        timeForAdd = cr.add(set,10000);
+        timeForDelete = cr.delete(set,100);
+        System.out.println("TreeSet");
         System.out.println("That took " + timeForAdd + " milliseconds for add operation");
-        System.out.println("That took " + timeForDelete+ " milliseconds for add operation");
+        System.out.println("That took " + timeForDelete+ " milliseconds for delete operation");
 
     }
 }
