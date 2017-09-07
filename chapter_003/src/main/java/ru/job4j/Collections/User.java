d0cce7c05c7adf7c4e8c4c71588b46358a77720f
valikir <v.turbin@gmx.de>
2017-09-07 21:24:23 +0200

    1. Организовать сортировку User [#10034]

--- chapter_003/src/main/java/ru/job4j/Collections/User.java
+++ chapter_003/src/main/java/ru/job4j/Collections/User.java
@@ -1,13 +1,45 @@
 package ru.job4j.Collections;
+import java.util.*;
 
-public class User {
+public class User implements Comparable<User> {
     public int id;
-    public String name;
-    public String city;
+    private String name;
+    private String city;
+    private int age;
 
-    public User(int id,String name,String city){
+
+    public User(int id, String name, int age, String city) {
         this.id = id;
         this.name = name;
         this.city = city;
+        this.age = age;
     }
+    @Override
+    public String toString(){
+        return "User{" + "id: " + id + " name: " + name + " age: " + age + " city: " + city + "}" + "\n" ;
+    }
+    @Override
+    public int compareTo(User o) {
+        return Integer.compare(this.age,o.age) ;
+    }
+static class SortUser{
+        public Set<User> sort(List<User> users){
+            Set<User> userSet = new TreeSet<>();
+           userSet.addAll(users);
+           return userSet;
+        }
 }
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.addAll(
+                Arrays.asList(
+                        new User(1, "Uasja", 14, "Astana"),
+                        new User(2, "Ashot", 12, "Ufa"),
+                        new User(3, "Evgenij", 13, "Praga")
+                )
+        );
+        SortUser sortUser = new SortUser();
+        System.out.println(sortUser.sort(users).toString());
+        }
+    }
+
