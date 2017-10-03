bcd193f5c1e4e5e305a6e8a138fdd124df1c7c5e
valikir <v.turbin@gmx.de>
2017-10-03 14:34:21 +0200

    5. Перекрывать и equals и hashCode [#1002]

--- chapter_004/src/main/java/Map/UserTest.java
+++ chapter_004/src/main/java/Map/UserTest.java
@@ -20,6 +20,18 @@ public class UserTest {
     }
 
         @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            User user = (User) o;
+
+            if (children != user.children) return false;
+            if (name != null ? !name.equals( user.name ) : user.name != null) return false;
+            return birthday != null ? birthday.equals( user.birthday ) : user.birthday == null;
+        }
+
+        @Override
         public int hashCode() {
             int result = name != null ? name.hashCode() : 0;
             result = 31 * result + children;
