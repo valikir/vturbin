988449059989d40cc6c4ceeebd3978f9de60c54b
valikir <v.turbin@gmx.de>
2017-11-22 13:57:58 +0100

    1. Создать элементарную структуру дерева [#1711]

--- chapter_004/src/main/java/Tree/Tree.java
+++ chapter_004/src/main/java/Tree/Tree.java
@@ -1,4 +1,4 @@
-package Tree;
+package tree;
 
 
 import java.util.*;
@@ -14,81 +14,82 @@ public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
             return value;
         }
 
-        public Node(List<Node<E>> children, E value){
+        public Node(List<Node<E>> children, E value) {
             this.value = value;
             this.children = children;
         }
     }
-    Node<E> parent1 = new Node<E>( new ArrayList<>(), (E) "parent" );
+    Node<E> parent1 = new Node<E>(new ArrayList<>(), (E) "parent");
 
 
-    private Node<E> SearchNode(E value, E childValue) {
-        Node PrevNode = parent1;
-        Node CurrentNode = parent1;
-        Node<E> DuplicateCheck = parent1;
+    private Node<E> searchNode(E value, E childValue) {
+        Node prevNode = parent1;
+        Node currentNode = parent1;
+        Node<E> duplicateCheck = parent1;
         int index = 0;
-        int ParentNodes = 0;
+        int parentNodes = 0;
 
-        while (CurrentNode.value != value) {
-            if (CurrentNode.children.size() > 1) {
-                CurrentNode.prev = PrevNode;
-                PrevNode = CurrentNode;
-                CurrentNode = (Tree.Node) CurrentNode.children.get( index );
-            } else if (CurrentNode.children.size() == 1) {
-                CurrentNode.prev = PrevNode;
-                PrevNode = CurrentNode;
+        while (currentNode.value != value) {
+            if (currentNode.children.size() > 1) {
+                currentNode.prev = prevNode;
+                prevNode = currentNode;
+                currentNode = (Tree.Node) currentNode.children.get(index);
+            } else if (currentNode.children.size() == 1) {
+                currentNode.prev = prevNode;
+                prevNode = currentNode;
                 index = 0;
-                CurrentNode = (Tree.Node) CurrentNode.children.get( index );
-            } else if (PrevNode.children.size() > index + 1) {
+                currentNode = (Tree.Node) currentNode.children.get(index);
+            } else if (prevNode.children.size() > index + 1) {
                 index++;
-                CurrentNode = (Tree.Node) PrevNode.children.get( index );
+                currentNode = (Tree.Node) prevNode.children.get(index);
             } else {
-                CurrentNode = PrevNode.prev;
-                ParentNodes++;
-                index = ParentNodes;
+                currentNode = prevNode.prev;
+                parentNodes++;
+                index = parentNodes;
             }
-            if (CurrentNode.children.size() > 1) {
-                DuplicateCheck  = (Tree.Node<E>) CurrentNode.children.get( index );
-            }
-                else if (CurrentNode.children.size() == 1){
-                    DuplicateCheck = (Tree.Node<E>) CurrentNode.children.get( 0 );
+            if (currentNode.children.size() > 1) {
+                duplicateCheck  = (Tree.Node) currentNode.children.get(index);
+            } else if (currentNode.children.size() == 1) {
+                    duplicateCheck = (Tree.Node) currentNode.children.get(0);
                 }
-                if (DuplicateCheck.value.equals( childValue )) {
-                    return CurrentNode = null;
+                if (duplicateCheck.value.equals(childValue)) {
+                    return null;
             }
         }
-        return CurrentNode;
+        return currentNode;
     }
 
 
     @Override
     public boolean add(E parent, Node<E> child) {
-        Node<E> AddNode = SearchNode( parent, child.value);
-       if (AddNode != null && AddNode.value.compareTo( parent )==0) {
-           AddNode.children.add( child );
-           child.prev = AddNode;
+        Node<E> addNode = searchNode(parent, child.value);
+       if (addNode != null) {
+           addNode.children.add(child);
+           child.prev = addNode;
            return true;
-       }
-        else {
+       } else {
            return false;
        }
     }
 
     @Override
     public Iterator iterator() {
-        List<E> Nodes = new ArrayList<>(  );
-        return null;
-    }
-
-    public static void main(String[] args){
-        Tree tree = new Tree<>();
-        tree.add("parent", new Node( new ArrayList<>(),"child" ));
-        tree.add("parent", new Node( new ArrayList<>(),"child2" ));
-        tree.add("child", new Node( new ArrayList<>(),"child4" ));
-        tree.add("child", new Node( new ArrayList<>(),"child4" ));
-        tree.add("child2", new Node( new ArrayList<>(),"child5" ));
-        tree.add("child2", new Node( new ArrayList<>(),"child6" ));
-        tree.add("child2", new Node( new ArrayList<>(),"child7" ));
+        List nodes = new ArrayList<>();
+        int index = 0;
+        nodes.add(parent1);
+        if (parent1.children.size() > 0){
+            for (Node node : parent1.children) {
+                nodes.add(node);
+                if (parent1.children.get(index).children != null) {
+                    Iterator childIterator = parent1.children.get(index).children.iterator();
+                    while(childIterator.hasNext()){
+                        nodes.add(childIterator.next());
+                    }
+                }
+                index++;
+            }
+        }
+        return nodes.iterator();
     }
 }
 
---
+++ chapter_004/src/test/java/tree/TreeTest.java
@@ -0,0 +1,40 @@
+package tree;
+
+import list.Node;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TreeTest {
+
+        @Test
+    public void whenAddSevenElementsDismissOneChild4() {
+            Tree tree = new Tree<>();
+            tree.add("parent", new Tree.Node(new ArrayList<>(), "child"));
+            tree.add("parent", new Tree.Node(new ArrayList<>(), "child2"));
+            tree.add("child", new Tree.Node(new ArrayList<>(), "child4"));
+            tree.add("child", new Tree.Node(new ArrayList<>(), "child4"));
+            tree.add("child2", new Tree.Node(new ArrayList<>(), "child5"));
+            tree.add("child2", new Tree.Node(new ArrayList<>(), "child6"));
+            tree.add("child2", new Tree.Node(new ArrayList<>(), "child7"));
+            Iterator itr = tree.iterator();
+            Tree.Node node = (Tree.Node) itr.next();
+            System.out.println(node.getValue());
+            Tree.Node node2 = (Tree.Node) itr.next();
+            System.out.println(node2.getValue());
+            Tree.Node node3 = (Tree.Node) itr.next();
+            System.out.println(node3.getValue());
+            Tree.Node node4 = (Tree.Node) itr.next();
+            System.out.println(node4.getValue());
+            Tree.Node node5 = (Tree.Node) itr.next();
+            System.out.println(node5.getValue());
+            Tree.Node node6 = (Tree.Node) itr.next();
+            System.out.println(node6.getValue());
+            Tree.Node node7 = (Tree.Node) itr.next();
+            System.out.println(node7.getValue());
+    }
+}
